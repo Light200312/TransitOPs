090985ce1cdf1e42a841de0098c8025318651cc5
Mohammad Arslan <arslan48950@gmail.com>
2026-07-12 11:48:24 +0530

    Setted up CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http://localhost:5174"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
